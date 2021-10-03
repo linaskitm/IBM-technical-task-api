be7feb638490c37d73c856a136dba1e240c19e73
Linas Pelenis <linaspelenis@yahoo.com>
2021-10-04 00:21:27 +0300

    configure to local use

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "us-cdbr-east-04.cleardb.com",
-  USER: "b63f40580c08ac",
-  PASSWORD: "e6e13a8e",
-  DB: "heroku_a136afda4515970",
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "admin",
+  DB: "ibm",
 };
